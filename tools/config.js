19b8b41589c120d71e4e582536f6e6042cbb04db
sergkhl <sergio.khlopenkov@gmail.com>
2015-09-05 16:46:03 +0300

    about component picture; readme urls;

--- tools/config.js
+++ tools/config.js
@@ -11,18 +11,18 @@ const SERVER = process.argv.includes('server');
 const STYLE_LOADER = 'style-loader/useable';
 const CSS_LOADER = DEBUG ? 'css-loader' : 'css-loader?minimize';
 const AUTOPREFIXER_BROWSERS = [
-    'Android 2.3',
-    'Android >= 4',
-    'Chrome >= 20',
-    'Firefox >= 24',
-    'Explorer >= 8',
-    'iOS >= 6',
-    'Opera >= 12',
-    'Safari >= 6'
+  'Android 2.3',
+  'Android >= 4',
+  'Chrome >= 20',
+  'Firefox >= 24',
+  'Explorer >= 8',
+  'iOS >= 6',
+  'Opera >= 12',
+  'Safari >= 6'
 ];
 const GLOBALS = {
-    'process.env.NODE_ENV': DEBUG ? '"development"' : '"production"',
-    '__DEV__': DEBUG
+  'process.env.NODE_ENV': DEBUG ? '"development"' : '"production"',
+  '__DEV__': DEBUG
 };
 
 //
@@ -31,126 +31,124 @@ const GLOBALS = {
 // -----------------------------------------------------------------------------
 
 const config = {
-    output: {
-        publicPath: './',
-        sourcePrefix: '  '
-    },
+  output: {
+    publicPath: './',
+    sourcePrefix: '  '
+  },
 
-    cache: DEBUG,
-    debug: DEBUG,
+  cache: DEBUG,
+  debug: DEBUG,
 
-    stats: {
-        colors: true,
-        reasons: DEBUG,
-        hash: VERBOSE,
-        version: VERBOSE,
-        timings: true,
-        chunks: VERBOSE,
-        chunkModules: VERBOSE,
-        cached: VERBOSE,
-        cachedAssets: VERBOSE
-    },
+  stats: {
+    colors: true,
+    reasons: DEBUG,
+    hash: VERBOSE,
+    version: VERBOSE,
+    timings: true,
+    chunks: VERBOSE,
+    chunkModules: VERBOSE,
+    cached: VERBOSE,
+    cachedAssets: VERBOSE
+  },
 
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin()
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin()
+  ],
+
+  resolve: {
+    root: [
+      path.join(__dirname, '../node_modules')
     ],
-
-    resolve: {
-        root: [
-            path.join(__dirname, '../node_modules')
-        ],
-        extensions: ['', '.webpack.js', '.web.js', '.js'],
-        modulesDirectories: [
-            'node_modules'
-        ]
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.txt/,
-                loader: 'file?name=[path][name].[ext]'
-            }, {
-                test: /\.js$/,
-                include: [
-                    path.resolve(__dirname, '../src')
-                ],
-                loaders: ['ng-annotate', 'babel-loader']
-            }
-        ]
-    },
-
-    postcss: [
-        require('postcss-nested')(),
-        require('cssnext')(),
-        require('autoprefixer-core')(AUTOPREFIXER_BROWSERS)
+    extensions: ['', '.webpack.js', '.web.js', '.js'],
+    modulesDirectories: [
+      'node_modules'
     ]
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.txt/,
+        loader: 'file?name=[path][name].[ext]'
+      }, {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, '../src')
+        ],
+        loaders: ['ng-annotate', 'babel-loader']
+      }
+    ]
+  },
+
+  postcss: [
+    require('postcss-nested')(),
+    require('cssnext')(),
+    require('autoprefixer-core')(AUTOPREFIXER_BROWSERS)
+  ]
 };
 
 //
 // Configuration for the client-side bundle (app.js)
 // -----------------------------------------------------------------------------
 const appConfig = merge({}, config, {
-    entry: {
-        app: [
-            ...(WATCH ? [
-                'webpack-hot-middleware/client'] : []),
-            './src/app.js'
-        ]
-    },
-    output: {
-        path: path.join(__dirname, '../www'),
-        filename: '[name].js',
-        chunkFilename: "[id].bundle.js"
-    },
-    devtool: DEBUG ? 'source-map' : false,
-    plugins: [
-        ...config.plugins,
-        new DefinePlugin(merge({}, GLOBALS, {'__SERVER__': false})),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'app',
-            minChunks: Infinity
-        }),
-        ...(DEBUG ? [] : [
-            new webpack.optimize.DedupePlugin(),
-            new webpack.optimize.UglifyJsPlugin({compress: {warnings: VERBOSE}}),
-            new webpack.optimize.AggressiveMergingPlugin()
-        ]),
-        ...(WATCH ? [
-            new webpack.HotModuleReplacementPlugin(),
-            new webpack.NoErrorsPlugin()
-        ] : [])
+  entry: {
+    app: [
+      ...(WATCH && ['webpack-hot-middleware/client']),
+      './src/app.js'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, '../www'),
+    filename: '[name].js',
+    chunkFilename: "[id].bundle.js"
+  },
+  devtool: DEBUG ? 'source-map' : false,
+  plugins: [
+    ...config.plugins,
+    new DefinePlugin(merge({}, GLOBALS, {'__SERVER__': false})),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'app',
+      minChunks: Infinity
+    }),
+    ...(!DEBUG && [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({compress: {warnings: VERBOSE}}),
+      new webpack.optimize.AggressiveMergingPlugin()
+    ]),
+    ...(WATCH && [
+      new webpack.HotModuleReplacementPlugin()
+    ])
+  ],
+  module: {
+    noParse: [
+      new RegExp('ionic.bundle.js'),
+      new RegExp('leaflet-src.js'),
+      new RegExp('photoswipe.js')
     ],
-    module: {
-      noParse: [
-        new RegExp('ionic.bundle.js'),
-        new RegExp('leaflet-src.js'),
-        new RegExp('photoswipe.js')
-      ],
-        loaders: [...config.module.loaders,
-            {
-                test: [/ionicons\.svg/, /ionicons\.eot/, /ionicons\.ttf/, /ionicons\.woff/],
-                loader: 'file?name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loader: 'url?limit=10000&name=img/[hash:7].[ext]!image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+    loaders: [...config.module.loaders,
+      {
+        test: [/ionicons\.svg/, /ionicons\.eot/, /ionicons\.ttf/, /ionicons\.woff/],
+        loader: 'file?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'url?limit=10000&name=img/[hash:7].[ext]!image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
 
-            }, {
-                test: /\.css$/,
-                loader: `style!${CSS_LOADER}`
-            }, {
-                test: /\.scss$/,
-                loader: `style!${CSS_LOADER}!sass`
-            }, {
-                test: /\.json$/,
-                loader: 'json'
-            }, {
-                test: /\.html$/,
-                loader: "ngtemplate?module=templates&relativeTo=" + (path.resolve(__dirname, '../src/') + "!html")
-            }
-        ]
-    }
+      }, {
+        test: /\.css$/,
+        loader: `style!${CSS_LOADER}`
+      }, {
+        test: /\.scss$/,
+        loader: `style!${CSS_LOADER}!sass`
+      }, {
+        test: /\.json$/,
+        loader: 'json'
+      }, {
+        test: /\.html$/,
+        loader: "ngtemplate?module=templates&relativeTo=" + (path.resolve(__dirname, '../src/') + "!html")
+      }
+    ]
+  }
 });
 
 //
@@ -159,52 +157,52 @@ const appConfig = merge({}, config, {
 
 let nodeModules = {};
 fs.readdirSync('node_modules')
-    .filter(function(x) {
-        return ['.bin'].indexOf(x) === -1;
-    })
-    .forEach(function(mod) {
-        nodeModules[mod] = mod;
-    });
+  .filter(function (x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function (mod) {
+    nodeModules[mod] = mod;
+  });
 
 const serverConfig = merge({}, config, {
-    entry: './src/server.js',
-    output: {
-        path: './build',
-        filename: 'server.js',
-        libraryTarget: 'commonjs2'
-    },
-    target: 'node',
-    externals: nodeModules,
-    /*externals: [
-        function (context, request, cb) {
-            var isExternal =
-                request.match(/^[a-z][a-z\/\.\-0-9]*$/i) &&
-                !request.match(/^google-auth-library/) &&
-                !context.match(/[\\/]google-auth-library/);
-            cb(null, Boolean(isExternal));
-        }
-    ],*/
-    node: {
-        console: false,
-        global: false,
-        process: false,
-        Buffer: false,
-        __filename: false,
-        __dirname: false
-    },
-    devtool: DEBUG ? 'source-map' : 'cheap-module-source-map',
-    plugins: [
-        ...config.plugins,
-        new DefinePlugin(merge({}, GLOBALS, {'__SERVER__': true})),
-        new BannerPlugin('require("source-map-support").install();',
-            {raw: true, entryOnly: false})
-    ],
-    module: {
-        loaders: [...config.module.loaders, {
-            test: /\.css$/,
-            loader: `${CSS_LOADER}!postcss-loader`
-        }]
-    }
+  entry: './src/server.js',
+  output: {
+    path: './build',
+    filename: 'server.js',
+    libraryTarget: 'commonjs2'
+  },
+  target: 'node',
+  externals: nodeModules,
+  /*externals: [
+   function (context, request, cb) {
+   var isExternal =
+   request.match(/^[a-z][a-z\/\.\-0-9]*$/i) &&
+   !request.match(/^google-auth-library/) &&
+   !context.match(/[\\/]google-auth-library/);
+   cb(null, Boolean(isExternal));
+   }
+   ],*/
+  node: {
+    console: false,
+    global: false,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false
+  },
+  devtool: DEBUG ? 'source-map' : 'cheap-module-source-map',
+  plugins: [
+    ...config.plugins,
+    new DefinePlugin(merge({}, GLOBALS, {'__SERVER__': true})),
+    new BannerPlugin('require("source-map-support").install();',
+      {raw: true, entryOnly: false})
+  ],
+  module: {
+    loaders: [...config.module.loaders, {
+      test: /\.css$/,
+      loader: `${CSS_LOADER}!postcss-loader`
+    }]
+  }
 });
 
 let resultConfig = SERVER ? [serverConfig] : [appConfig, serverConfig];
